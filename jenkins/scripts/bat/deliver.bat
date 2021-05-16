1397468415bfced7313e70df5d1ed19f203c5038
Lakshman Veti <lakshman.veti@gmail.com>
2021-05-16 11:47:30 +0530

    test delivery

--- jenkins/scripts/bat/deliver.bat
+++ jenkins/scripts/bat/deliver.bat
@@ -4,7 +4,7 @@ echo '"/var/jenkins_home/workspace/simple-node-js-react-app" directory),'
 echo 'correctly bundles React in production mode and optimizes the build for'
 echo 'the best performance.'
 set -x
-npm build && npm start
+npm build && npm start &
 set +x
 echo 'The following "npm" command runs your Node.js/React application in'
 echo 'development mode and makes the application available for web browsing.'
